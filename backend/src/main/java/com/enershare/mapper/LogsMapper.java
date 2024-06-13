aca5f617cc2824c2db10ec2d164994ec5edfdaae
vukeurodyn <vuk.topalovic@eurodyn.com>
2024-06-13 11:03:57 +0200

    fixed mapper for requestparameters

--- backend/src/main/java/com/enershare/mapper/LogsMapper.java
+++ backend/src/main/java/com/enershare/mapper/LogsMapper.java
@@ -7,24 +7,54 @@ import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 import org.mapstruct.NullValueCheckStrategy;
 
-import java.util.Map;
-
 @Mapper(componentModel = "spring", nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
-public interface LogsMapper {
+public abstract class LogsMapper {
     @Named("getPurposeFromContext")
-    static String getPurposeFromContext(Map<String, Object> contextParameters) {
-        return (String) contextParameters.get("purpose");
+    public String getPurposeFromContext(LogsDTO logsDTO) {
+        if (logsDTO.getContextParameters() != null) {
+            return (String) logsDTO.getContextParameters().get("purpose");
+        }
+        return null;
     }
 
     @Named("getDatClaimsFromContext")
-    static String getDatClaimsFromContext(Map<String, Object> contextParameters) {
-        return (String) contextParameters.get("datClaims");
+    public String getDatClaimsFromContext(LogsDTO logsDTO) {
+        if (logsDTO.getContextParameters() != null) {
+            return (String) logsDTO.getContextParameters().get("datClaims");
+        }
+        return null;
     }
 
-    @Mapping(target = "purpose", source = "contextParameters", qualifiedByName = "getPurposeFromContext")
-    @Mapping(target = "datClaims", source = "contextParameters", qualifiedByName = "getDatClaimsFromContext")
-    Logs mapConsumerDTOToEntity(LogsDTO logsDTO);
+    @Named("getResourceIdFromRequestParameters")
+    public String getResourceIdFromRequestParameters(LogsDTO logsDTO) {
+        if (logsDTO.getRequestParameters() != null) {
+            return (String) logsDTO.getRequestParameters().getResourceId();
+        }
+        return null;
+    }
 
-    LogsDTO mapEntityToDTO(Logs logs);
+    @Named("getResourceTypeFromRequestParameters")
+    public String getResourceTypeFromRequestParameters(LogsDTO logsDTO) {
+        if (logsDTO.getRequestParameters() != null) {
+            return (String) logsDTO.getRequestParameters().getResourceType();
+        }
+        return null;
+    }
+
+    @Named("getActionFromRequestParameters")
+    public String getActionFromRequestParameters(LogsDTO logsDTO) {
+        if (logsDTO.getRequestParameters() != null) {
+            return (String) logsDTO.getRequestParameters().getAction();
+        }
+        return null;
+    }
+
+    @Mapping(target = "purpose", source = "logsDTO", qualifiedByName = "getPurposeFromContext")
+    @Mapping(target = "datClaims", source = "logsDTO", qualifiedByName = "getDatClaimsFromContext")
+    @Mapping(target = "resourceId", source = "logsDTO", qualifiedByName = "getResourceIdFromRequestParameters")
+    @Mapping(target = "resourceType", source = "logsDTO", qualifiedByName = "getResourceTypeFromRequestParameters")
+    @Mapping(target = "action", source = "logsDTO", qualifiedByName = "getActionFromRequestParameters")
+    public abstract Logs mapConsumerDTOToEntity(LogsDTO logsDTO);
+
+    public abstract LogsDTO mapEntityToDTO(Logs logs);
 }
-
